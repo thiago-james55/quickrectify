72819cd59baa00f18d5a8cd4b74a742482e96f10
Thiago Gonçalves <tsyhwh55@gmail.com>
2023-07-02 19:57:41 -0300

    BACK - Projeto criado

---
+++ backend/src/main/java/com/quickrectify/quickrectify/QuickrectifyApplication.java
@@ -0,0 +1,13 @@
+package com.quickrectify.quickrectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickrectifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickrectifyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/quickrectify/quickrectify/QuickrectifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickrectify.quickrectify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickrectifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
