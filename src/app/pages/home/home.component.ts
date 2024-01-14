a13e69f762b2de26f56d44dafccb4333abdd2162
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-13 21:24:20 -0300

    home-page-component created!

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+
+@Component({
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [NavbarComponent]
+})
+export class HomeComponent {
+
+}
