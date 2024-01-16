3954f7fcff8df38aa9e97716179a50502576ddf5
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-15 21:02:05 -0300

    Layout changes and home component improvements!

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [NavbarComponent]
+    imports: [NavbarComponent, FooterComponent]
 })
 export class HomeComponent {
 
