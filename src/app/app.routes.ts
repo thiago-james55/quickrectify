919237717bf03e702a514f859fec2e633680ea48
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-17 20:27:51 -0300

    Layout e project structures changes!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NewOrderComponent } from './pages/new-order/new-order.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent
+    }, {
+        path: 'neworder',
+        component:NewOrderComponent
+    }
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { PageTitleComponent } from "../../components/page-title/page-title.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [NavbarComponent, FooterComponent]
+    styleUrls: ['./home.component.css', '../../../global.css'],
+    imports: [NavbarComponent, FooterComponent, PageTitleComponent]
 })
 export class HomeComponent {
 
