01fb65c8c6f247b938cc9b2149506ccaf6365f2e
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-19 20:01:30 -0300

    Layout e structure for view testing!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
     {
         path:'',
-        component:HomeComponent
+        component:NewOrderComponent
     }, {
         path: 'neworder',
         component:NewOrderComponent
---
+++ src/app/service/model/Part.entity.ts
@@ -0,0 +1,22 @@
+export interface Part {
+    name: string;
+    services: string[]
+}
+ 
+ export const parts: Part[] = [
+    { name : "Biela" , services : ["Banho","Completa","Só Ferro","Só Bucha","Montar Pistão"] },
+    { name : "Bloco" , services : ["Banho","Abrir","Encamisar","Plainar","Soldar","Mandrilhar","Trocar Bucha"] },
+    { name : "Cabeçote" , services : ["Banho","Plainar","Soldar","Mandrilhar","Completo","Regular"] },
+    { name : "Virabrequim" , services : ["Banho","Retificar","Encher Lateral","Polir"] },
+    { name : "Volante" , services : ["Banho","Retificar","Virar Gremalheira"] },
+    { name : "Outros" , services : ["Solda Ferro","Solda Aluminio","Solda Cart"] },
+];
+
+export function getParts() : Part[] {
+
+    parts.forEach(e => {
+        e.services.sort();
+    });
+
+    return parts;
+}
