dda898970e8efc3f98ff522047cd21e9401af0f0
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-22 21:40:10 -0300

    General Project Structure Improvements!

--- src/app/service/model/Part.entity.ts
+++ src/app/service/model/Part.entity.ts
@@ -1,6 +1,15 @@
 export interface Part {
     name: string;
-    services: string[]
+    services: string[];
+}
+
+export interface OrderPart {
+    name: string;
+    service?: string;
+    description?: string;
+    quantity?: number;
+    pricePerQuantity?: number;
+    priceTotal?: number;
 }
  
  export const parts: Part[] = [
