0bfef6743a1b4606334a857f4a46ebb642f767d9
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-01-30 20:45:07 -0300

    NoComma pipe created!

---
+++ src/app/pipes/no-comma.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'noComma',
+  standalone: true
+})
+export class NoCommaPipe implements PipeTransform {
+
+  transform(val: string | null ): string {
+    if (val !== undefined && val !== null) {
+      return val.toString().replace(/,/g, "");
+    } else {
+      return "";
+    }
+  }
+
+}
