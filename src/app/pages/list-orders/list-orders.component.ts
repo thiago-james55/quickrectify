b8ee5225860f4148431ebab06cae51dbd3a6606c
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-02-01 19:13:54 -0300

    List-Orders page created!

---
+++ src/app/pages/list-orders/list-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { PageTitleComponent } from "../../components/page-title/page-title.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+
+@Component({
+    selector: 'app-list-orders',
+    standalone: true,
+    templateUrl: './list-orders.component.html',
+    styleUrls: ['./list-orders.component.css','../../../global.css'],
+    imports: [NavbarComponent, PageTitleComponent, FooterComponent]
+})
+export class ListOrdersComponent {
+
+}
