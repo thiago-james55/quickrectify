978aeac2a4e620396fed42bd6e1586e7a07d5b1e
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-02-02 18:54:13 -0300

    List-Orders-Handler component created!

---
+++ src/app/components/list-orders-handler/list-orders-handler.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-orders-handler',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-orders-handler.component.html',
+  styleUrls: ['./list-orders-handler.component.css', '../../../global.css']
+})
+export class ListOrdersHandlerComponent {
+
+}
