0c3178a6877da985bd74802bc43dff39bdfa8e82
Thiago Gonçalves <87917583+thiago-james55@users.noreply.github.com>
2024-06-26 15:49:37 -0300

    Update OrderDTO.cs
    Order DiscountCash and DiscountPercent bug solve!

--- Models/DTO/OrderDTO.cs
+++ Models/DTO/OrderDTO.cs
@@ -34,8 +34,8 @@ namespace QuickRectify.Models.DTO
         {
             Id = order.Id;
             Date = order.Date;
-            DiscountPercent = order.DiscountPercent.HasValue ? DiscountPercent : 0;
-            DiscountCash = order.DiscountCash.HasValue ? DiscountCash : 0;
+            DiscountPercent = order.DiscountPercent ?? order.DiscountPercent : 0;
+            DiscountCash = order.DiscountCash ?? order.DiscountCash : 0;
             PriceSubTotal = order.PriceSubTotal;
             PriceTotal = order.PriceTotal;
             ConsumerId = order.ConsumerId;
