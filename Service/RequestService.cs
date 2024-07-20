56797c9498e4d94690561a031dad8e6e5cbf83cc
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-07-20 12:28:33 -0300

    RequestService class created!

---
+++ Service/RequestService.cs
@@ -0,0 +1,38 @@
+﻿using QuickRectifyMaui2.Utils;
+using System.Net;
+
+public static class RequestService
+{
+    static readonly HttpClient _httpClient = new HttpClient();
+
+    public static async Task<bool> ServerIsValid(String? ipAddress, String? ipPort)
+    {
+        if (ipAddress == null || ipPort == null) 
+        {
+            ipAddress = Preferences.Get("serverIpAddress", String.Empty);
+            ipPort = Preferences.Get("serverIpPort", String.Empty);
+        }
+
+
+        if ( !(ipAddress != String.Empty && ipPort != String.Empty) )
+        {
+            await ToastMessage.ShowToastMessage("IP não configurado!");
+            return false;
+        }
+
+        var serverUrl = $"http://{ipAddress}:{ipPort}";
+
+        try
+        {
+            var response = await _httpClient.GetAsync(serverUrl + "/swagger"); //Change to healthy
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            await ToastMessage.ShowToastMessage(ex.Message);
+            return false;
+        }
+
+    }
+
+}
