28de578c0309b765b2b427b6da9b19267b5f6b81
Thiago Gonçalves <tsyhwh55@gmail.com>
2024-07-20 12:28:01 -0300

    ToastMessage class created!

---
+++ Utils/ToastMessage.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace QuickRectifyMaui2.Utils
+{
+    public static class ToastMessage
+    {
+        public static async Task ShowToastMessage(string message)
+        {
+            ToastDuration duration = ToastDuration.Short;
+
+            double fontSize = 14;
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            var toast = Toast.Make(message, duration, fontSize);
+
+            await toast.Show(cancellationTokenSource.Token);
+        }
+
+    }
+}
